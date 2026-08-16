43af68745fe2df404e596c4d4f5a762d9f748c70
carlossano888-create <carlossano888@gmail.com>
2026-08-15 23:51:53 -0600

    Crear ale.lua

---
+++ ale.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY = "SIN LLAVE"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/c76f10faea491e95b3da11348d92bd1b611f6b1d196d18743ea1935b4ea8f624/download"))()
