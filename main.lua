bc287b7ad46a9d80d614dc6e208993a458cf60e6
carlossano888-create <carlossano888@gmail.com>
2026-09-12 02:42:06 -0600

    Actualizar main.lua

--- main.lua
+++
@@ -1,2 +0,0 @@
-getgenv().SCRIPT_KEY = "SIN LLAVE"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/793973c38237c320e228c9a457645c5513a6d76183637727e893bf202fb7ae96/download"))()
