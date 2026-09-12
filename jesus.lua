96226bb54def14e4d375e8dd7e9336d369ad65e3
carlossano888-create <carlossano888@gmail.com>
2026-09-12 02:41:51 -0600

    Actualizar jesus.lua

--- jesus.lua
+++ jesus.lua
@@ -1,2 +1,1 @@
-getgenv().SCRIPT_KEY = "SIN LLAVE"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/93b109a92c21216dadba822719e696e1ac3a68b86f539acbaa7f91a69dbf3c9f/download"))()
+ñ
